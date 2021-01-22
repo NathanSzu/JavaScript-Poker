8ee50f29945bd7511eccbcff8a5c2fb680adb267
Nathan Szurek <nathan.szurek@yahoo.com>
2021-01-21 18:00:56 -0600

    Changed data types for card deck and began function to check hand rank

--- handRankings.js
+++
@@ -1,8 +0,0 @@
-export default checkHandRank = (playerHand) => {
-    var suits = []
-    var values = []
-    for (let i = 0; i < array.length; i++) {
-        
-        
-    }
-}
--- poker.js
+++ poker.js
@@ -20,8 +20,6 @@ var playerHands = [
 var playerCount = 4;
 
 
-// console.log(cards.deck)
-
 var drawCard = () => {
     var card = null;
     if (cards.deck.length === 0) {
@@ -29,7 +27,6 @@ var drawCard = () => {
     } else {
         card = cards.deck[Math.floor(Math.random() * cards.deck.length)];
         cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1)
-        console.log('cards.deck', cards.deck)
         return card
     }
 }
@@ -48,12 +45,54 @@ var discard = () => {
     }
 }
 
+
+var checkHandRank = (playerHand) => {
+    var suits = []
+    var values = []
+
+    for (let i = 0; i < playerHand.length; i++) {
+        suits.push(playerHand[i].suit)
+        values.push(playerHand[i].value)
+    }
+
+    // Condensed.length will be used to help check hands
+    var condensedValues = [...new Set(values)];
+    var condensedSuits = [...new Set(suits)];
+    var sequentialCheck = () => {
+        values.sort()
+        let output = true;
+        for (let i = 0; i < values.length; i++) {
+            if (values[i] === ) {
+                
+            } else
+            
+        }
+    }
+
+    if (condensedValues.length === 5 && values.includes('14') &&) {
+        
+    } else {
+        
+    }
+
+
+
+    console.log('Suits: ', suits);
+    console.log('Values: ', values);
+    console.log('Hand: ', playerHand);
+    console.log('Condensed Suits: ', condensedSuits)
+}
+
+
 var playGame = () => {
+    // Start by 'dealing' cards to each player based on desired hand size and # of players
     for (let x = 0; x < handSize; x++) {
         for (let i = 0; i < playerCount; i++) {
             playerHands[i].push(drawCard())
         }
     }
+
+    // Function to check for winning hand
 }
 
 var showHands = () => {
@@ -67,5 +106,5 @@ var showHands = () => {
 }
 
 playGame()
-showHands()
-console.log(cards.deck)
+// showHands()
+checkHandRank(playerHands[0])
